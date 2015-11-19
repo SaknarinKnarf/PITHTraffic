b6d878750421b71d4f765e3e1c95c6867345b22d
Saknarin.P <saknarin.p@thaiplastics.org>
2015-11-19 15:56:21 +0700

    Day 1 Finish

--- app/src/main/java/pith/itdev/pithtraffic/MainActivity.java
+++ app/src/main/java/pith/itdev/pithtraffic/MainActivity.java
@@ -1,8 +1,12 @@
 package pith.itdev.pithtraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.provider.LiveFolders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -21,8 +25,31 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
 
     }   //Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttunMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.horse); //getBase:จากภายใน getMedia:จากภายนอก
+                buttunMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/HWb1m5PM6zI"));
+                startActivity(objIntent);
+
+            }   //event
+        });
+
+    }
 
     private void bindWidget() {
 
